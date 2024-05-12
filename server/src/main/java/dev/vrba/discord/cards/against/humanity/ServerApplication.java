5ac149516de37ecda61f7958243b573ebb0ebcf9
Jiří Vrba <jiri@vrba.dev>
2024-05-12 14:24:20 +0200

    Create a new Spring Boot application for the server

---
+++ server/src/main/java/dev/vrba/discord/cards/against/humanity/ServerApplication.java
@@ -0,0 +1,13 @@
+package dev.vrba.discord.cards.against.humanity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
